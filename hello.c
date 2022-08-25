ab35477437b6e98208557208ce9c36dfcf6279db
Joseph Kottwitz <jkottwitz3@huskers.unl.edu>
2022-08-25 18:52:30 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Joseph Kottwitz
+ *         jkottwitz@unomaha.edu
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Joseph Kottwitz");
+
+  return 0;
+}
